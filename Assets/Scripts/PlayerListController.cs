c015b543cdbbdf246f7539555936916a20e78521
CasualBot <sbernard199@gmail.com>
2020-11-18 16:22:21 -0600

    wip

--- Assets/Scripts/PlayerListController.cs
+++ Assets/Scripts/PlayerListController.cs
@@ -7,29 +7,4 @@ public class PlayerListController : MonoBehaviour
 {
     public List<GameObject> playersList;
 
-    void Awake()
-    {
-        foreach (var player in playersList)
-        {
-            player.transform.Find("PlayerName").GetComponent<UnityEngine.UI.Text>().text = "";
-        }
-    }
-
-    void Update()
-    {
-        UpdatePlayerList();
-    }
-
-    private void UpdatePlayerList()
-    {
-        var networkList = PhotonNetwork.PlayerList;
-
-        if (networkList.Length > 0)
-        {
-            // for (var i = 0; i < networkList.Length; i++)
-            // {
-            //     Debug.LogFormat("Player: {0}", networkList[i].NickName);   
-            // }
-        }
-    }
 }
